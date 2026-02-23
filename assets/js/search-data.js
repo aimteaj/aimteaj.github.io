303a722b4d886198c6dc465dc52782ea914b6875
aimteaj <aimteaj@fau.edu>
2026-02-23 03:42:38 +0000

    Deploying to gh-pages from @ aimteaj/aimteaj.github.io@1d751a360994b71a15a947b5d1f4c7e874172223 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-research-projects",
           title: "Research Projects",
           description: "Selected research highlights and publications",
