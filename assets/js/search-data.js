6fb4f58b6be640b4974eddb1089ab0cf0d4267fa
aimteaj <aimteaj@fau.edu>
2026-03-22 05:54:16 +0000

    Deploying to gh-pages from @ aimteaj/aimteaj.github.io@7730ce9b7bfee99bcb4ec6459177d362dffa9ad5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -116,7 +116,7 @@ ninja.data = [{
           section: "News",},{id: "news-cvpr-2026-paper-accepted-our-paper-when-data-is-scarce-learn-to-adapt-robust-federated-learning-via-adversarial-meta-optimization-has-been-accepted-in-the-findings-track-of-the-ieee-cvf-conference-on-computer-vision-and-pattern-recognition-cvpr-2026-ranked-1-cs-conference-project-page",
           title: 'CVPR 2026 Paper Accepted Our paper, “When Data is Scarce, Learn to Adapt:...',
           description: "",
-          section: "News",},{id: "news-ijcnn-2026-paper-accepted-our-paper-sim-clip-unsupervised-siamese-adversarial-fine-tuning-for-robust-and-semantically-rich-vision-language-models-has-been-accepted-in-the-main-track-of-the-ieee-international-joint-conference-on-neural-networks-ijcnn-2026-rank-a-cs-conference-project-page",
+          section: "News",},{id: "news-ijcnn-2026-paper-accepted-our-paper-sim-clip-unsupervised-siamese-adversarial-fine-tuning-for-robust-and-semantically-rich-vision-language-models-has-been-accepted-in-the-main-track-of-the-ieee-international-joint-conference-on-neural-networks-ijcnn-2026-ranked-a-cs-conference-project-page",
           title: 'IJCNN 2026 Paper Accepted Our paper, “Sim-CLIP: Unsupervised Siamese Adversarial Fine-Tuning for Robust...',
           description: "",
           section: "News",},{id: "projects-project-1",
