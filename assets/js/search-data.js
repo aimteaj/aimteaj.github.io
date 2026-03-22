30622b4d89bc385d409e31a30310e4853f01acba
aimteaj <aimteaj@fau.edu>
2026-03-22 18:48:18 +0000

    Deploying to gh-pages from @ aimteaj/aimteaj.github.io@21b5e1865c90ed5e1ce584f18c29086f7f604bea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-grants-amp-awards",
           title: "Grants &amp; Awards",
-          description: "Research grants and academic honors",
+          description: "Research grants, honors, and academic recognitions",
           section: "Navigation",
           handler: () => {
             window.location.href = "/grants-awards/";
